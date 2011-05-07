2357523122da0e5fb1ca23e97e71612f57a7e555
Andrew Kelley <superjoe30@gmail.com>
2011-05-06 17:32:34 -0700

    adding pros and cons works

--- main/views.py
+++ main/views.py
@@ -10,13 +10,24 @@ from main.models import TruthNode, NodeRelationship
 from main.forms import CreateNodeForm
 
 def home(request):
-    nodes = TruthNode.objects.all()
+    nodes = [n for n in TruthNode.objects.all() if NodeRelationship.objects.filter(child_node__pk=n.pk).count() == 0]
     return render_to_response('home.html', {'nodes': nodes}, 
         context_instance=RequestContext(request))
 
 def node(request, node_id):
     node = get_object_or_404(TruthNode, pk=int(node_id))
-    return render_to_response('node.html', {'node': node}, 
+    children_rels = NodeRelationship.objects.filter(parent_node__pk=node.pk)
+    pro_rels = children_rels.filter(relationship=NodeRelationship.PRO)
+    con_rels = children_rels.filter(relationship=NodeRelationship.CON)
+    pros = [rel.child_node for rel in pro_rels]
+    cons = [rel.child_node for rel in con_rels]
+
+    context = {
+        'node': node,
+        'pros': pros,
+        'cons': cons,
+    }
+    return render_to_response('node.html', context, 
         context_instance=RequestContext(request))
 
 def add_node(request):
@@ -63,8 +74,36 @@ def delete_node(request, node_id):
         return render_to_response('delete.html', {'node': node}, 
             context_instance=RequestContext(request))
 
+def add_arg(request, node_id, arg_type):
+    parent = get_object_or_404(TruthNode, pk=int(node_id))
+    if request.method == 'POST':
+        form = CreateNodeForm(request.POST)
+        if form.is_valid():
+            node = TruthNode()
+            node.title = form.cleaned_data.get('title')
+            node.content = form.cleaned_data.get('content')
+            node.save()
+
+            relate = NodeRelationship()
+            relate.parent_node = parent
+            relate.child_node = node
+            relate.relationship = arg_type
+            relate.save()
+
+            return HttpResponseRedirect(reverse('node', args=[parent.id]))
+    else:
+        form = CreateNodeForm()
+
+    context = {
+        'parent': parent,
+        'arg_type': arg_type,
+        'form': form,
+    }
+    return render_to_response('arg.html', context,
+        context_instance=RequestContext(request))
+
 def add_pro(request, node_id):
-    pass
+    return add_arg(request, node_id, NodeRelationship.PRO)
 
 def add_con(request, node_id):
-    pass
+    return add_arg(request, node_id, NodeRelationship.CON)
