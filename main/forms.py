9070916a7acd52449b4903acad54ca47c711997e
Andrew Kelley <superjoe30@gmail.com>
2011-05-07 02:44:27 -0700

    allow some more html tags through for rich text

--- main/forms.py
+++ main/forms.py
@@ -2,6 +2,37 @@ from django import forms
 from main.models import TruthNode
 import bleach
 
+allowed_tags = [
+    'a',
+    'abbr',
+    'acronym',
+    'b',
+    'blockquote',
+    'code',
+    'em',
+    'i',
+    'li',
+    'ol',
+    'strong',
+    'ul',
+    'br',
+    'font',
+    'span',
+    'div',
+]
+
+allowed_attrs = {
+    'a': ['href', 'title'],
+    'abbr': ['title'],
+    'acronym': ['title'],
+    'span': ['style'],
+}
+
+allowed_styles = [
+    'font-size',
+    'line-height',
+]
+
 class CreateNodeForm(forms.ModelForm):
     class Meta:
         model = TruthNode
@@ -9,4 +40,5 @@ class CreateNodeForm(forms.ModelForm):
 
     def clean_content(self):
         content = self.cleaned_data['content']
-        return bleach.clean(content)
+        return bleach.clean(content, tags=allowed_tags,
+            attributes=allowed_attrs, styles=allowed_styles)
