d002435feebee97d96a34db918714b08a251f88f
Andrew Kelley <superjoe30@gmail.com>
2011-09-01 03:29:36 -0700

    expand [[node 23]] links

--- main/templatetags/hash_filter.py
+++ main/templatetags/hash_filter.py
@@ -11,3 +11,33 @@ def hash(attr, obj):
         value = None
 
     return value
+
+
+import re
+from main.models import TruthNode
+from django.core.urlresolvers import reverse
+from django.utils.safestring import mark_safe
+
+node_expansion_re = re.compile(r'\[\[node (\d+)\]\]')
+invalid_text = 'Unknown Claim'
+html_invalid_text = '<span style="text-decoration: underline">%s</span>' % invalid_text
+@register.filter
+def expand_node_titles(text, hyperlink=False):
+    def get_expansion(match):
+        try:
+            node_id = int(match.group(1))
+        except ValueError:
+            return html_invalid_text
+        try:
+            title = '{%s}' % TruthNode.objects.get(pk=node_id).title
+        except TruthNode.DoesNotExist:
+            return html_invalid_text
+        if hyperlink:
+            return '<a href="%s">%s</a>' % (reverse('node', args=[node_id]), title)
+        else:
+            return title
+    return mark_safe(node_expansion_re.sub(get_expansion, text))
+
+@register.filter
+def expand_node_links(text):
+    return expand_node_titles(text, hyperlink=True)
